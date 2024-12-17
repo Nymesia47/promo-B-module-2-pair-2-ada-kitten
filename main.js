17c561206a0cd7498d5aa1d63efea9757ef0c943
MelsCrest <melisacresta@gmail.com>
2024-12-17 11:35:07 +0100

    converted variables into objects

--- main.js
+++ main.js
@@ -13,39 +13,59 @@ const searchButton = document.querySelector('.js_button-search');
 
 //TARJETAS GATOS
 //GATO UNO
-const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
-const kittenName1 = 'Anastasio';
-const kittenDesc1 = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenRac1 = 'Siamés';
+const kittenData_1 ={
+    image : "https://dev.adalab.es/gato-siames.webp",
+    name : 'Anastasio',
+    desc : 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+    race : 'Siamés'
+};
 //GATO DOS
-const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
-const kittenName2 = 'Fiona';
-const kittenDesc2 = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
-const kittenRac2 = 'Sphynx';
+const kittenData_2 ={
+    image : "https://dev.adalab.es/sphynx-gato.webp",
+    name : 'Fiona',
+    desc : 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+    race : 'Sphynx'
+};
 //GATO TRES
-const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
-const kittenName3 = 'Cielo';
-const kittenDesc3 = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.';
-const kittenRac3 = 'Maine Coon';
+const kittenData_3 ={
+    image : "https://dev.adalab.es/maine-coon-cat.webp",
+    name : 'Cielo',
+    desc : 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+    race : 'Maine Coon' 
+};
 
-function renderKitten(url, name, race, desc){
-    let kittenImage = url;
-    let kittenName = name;
-    let kittenRace = race;
-    let kittenDesc = desc;
-    let NewCat = `<li class="card">
+function renderKitten(kittenData){
+    let kittenImage = kittenData["image"];
+    let kittenName = kittenData["name"];
+    let kittenRace = kittenData["race"];
+    let kittenDesc = kittenData["desc"];
+    kittenRace = renderRace(kittenData["race"]); //Llamar a la función comprobar raza
+
+    let newCat = `<li class="card">
                     <img class="card_img" src="${kittenImage}" alt="sphynx-cat" />
                     <h3 class="card_title">${kittenName.toUpperCase()}</h3>
                     <h4 class="card_race js-kitten-race">${kittenRace}</h4>
                     <p class="card_description">${kittenDesc}</p>
                 </li>`;
-    return NewCat;
-
+    
+    return newCat;
 };
 
-const kittenOne = renderKitten(kittenImage1, kittenName1, kittenRac1, kittenDesc1);
-const kittenTwo = renderKitten(kittenImage2, kittenName2, kittenRac2, kittenDesc2);
-const kittenThree = renderKitten(kittenImage3, kittenName3, kittenRac3, kittenDesc3);
+//COMPROBAR SI HAY RAZA
+//1 - seleccionar elementos HTML: h4 (raza).
+//2 - comprobar contenido if/else.
+//3 - pintar resultado. 
+function renderRace (kittenRace) {
+    if (kittenRace === "") {
+        return `Uy que despiste, no sabemos su raza`;
+    } else {
+        return kittenRace;
+    };
+};
+
+const kittenOne = renderKitten(kittenData_1);
+const kittenTwo = renderKitten(kittenData_2);
+const kittenThree = renderKitten(kittenData_3);
 
 catList.innerHTML = catList.innerHTML + kittenOne + kittenTwo + kittenThree;
 
@@ -88,45 +108,43 @@ function filterKitten(ev){
 
     if ( descrSearchText !== '' ) {
 
-          catList.innerHTML = '';
+        catList.innerHTML = '';
 
-        if (kittenDesc1.includes(descrSearchText)){
+        if (kittenData_1["desc"].includes(descrSearchText)){
         
             catList.innerHTML = kittenOne;
 
         }
     
-        if (kittenDesc2.includes(descrSearchText)) {
+        if (kittenData_2["desc"].includes(descrSearchText)) {
 
             catList.innerHTML += kittenTwo;
         
         }
     
-        if (kittenDesc3.includes(descrSearchText)) {
+        if (kittenData_3["desc"].includes(descrSearchText)) {
 
             catList.innerHTML += kittenThree;
         
         }
-
-
     }
 
     if ( raceSearchText !== '') {
         catList.innerHTML = '';
 
-        if (kittenRac1.includes(raceSearchText)){
+        if (kittenData_1["race"].includes(raceSearchText)){
         
             catList.innerHTML = kittenOne;
 
         }
     
-        if (kittenRac2.includes(raceSearchText)) {
+        if (kittenData_2["race"].includes(raceSearchText)) {
 
             catList.innerHTML += kittenTwo;
         
         }
     
-        if (kittenRac3.includes(raceSearchText)) {
+        if (kittenData_3["race"].includes(raceSearchText)) {
 
             catList.innerHTML += kittenThree;
         
@@ -139,29 +157,3 @@ function filterKitten(ev){
 
 searchButton.addEventListener('click', filterKitten);
 
-//COMPROBAR SI HAY RAZA
-//1 - seleccionar elementos HTML: h4 (raza).
-//2 - comprobar contenido if/else.
-//3 - pintar resultado. 
-
-//let breedText = "";
-
-const kittenRace = document.querySelector('.js-kitten-race');
-
-
-function renderRace (race) {
-    if (race === "") {
-        return `Uy que despiste, no sabemos su raza`;
-    } else {
-        return race;
-    };
-}
-
-kittenRace.innerHTML = renderRace(kittenRace);
-
-// let breedText = "";
-// const kittenRace = document.querySelector('.js-kitten-race');
-// if (kittenRace.innerHTML === "") {
-//     breedText = `Uy que despiste, no sabemos su raza`;
-//     kittenRace.innerHTML = breedText;
-// };
